9cd64096b694d5d70f05d117df51fa18d773bb22
Preda Andrei <preda.andrei174@gmail.com>
2020-02-06 00:34:30 +0200

    Repara dependinte pentru instaleaza.sh

--- instaleaza.sh
+++ instaleaza.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
+# Instaleaza curl.
+apt install curl
+
+# Instaleaza o versiune mai noua de Vim.
+add-apt-repository ppa:jonathonf/vim
+apt update
+apt install vim
+
 # Instaleaza vim-plug.
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
