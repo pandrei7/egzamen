636927c359bed3dc5c442a420487ac1ed20af585
Preda Andrei <preda.andrei174@gmail.com>
2020-02-05 23:38:04 +0200

    Adauga comanda pentru instalare ag

--- instaleaza.sh
+++ instaleaza.sh
@@ -3,3 +3,6 @@
 # Instaleaza vim-plug.
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# Instaleaza ag.
+sudo apt install silversearcher-ag
